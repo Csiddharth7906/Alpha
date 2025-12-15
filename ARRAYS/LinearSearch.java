58a498048603a73baa79a16d1c2e9a2df63a5c5a
Siddharth Chauhan <158046791+Csiddharth7906@users.noreply.github.com>
2025-12-15 18:15:52 +0530

    linear search

--- ARRAYS/LinearSearch.java
+++ ARRAYS/LinearSearch.java
@@ -1,3 +1,15 @@
 public class LinearSearch {
-    
+    public static int Search(int nums[],int key){
+        for (int i = 0; i < nums.length; i++) {
+            if(nums[i]==key){
+                return i;
+            }
+        }
+        return -1;
+    }
+    public static void main(String[] args) {
+        int []arr={2,5,8,3,23,34,32,45};
+        int key=34;
+        System.out.println("THe psition of the key "+key+" value is "+Search(arr, key));
+    }
 }
